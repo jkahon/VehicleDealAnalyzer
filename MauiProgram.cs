a32adf78fd073b74466181591bb62255a519a26b
Jeff Kahon <jeff.kahon@gmail.com>
2026-08-23 00:26:11 -0500

    Create MauiProgram.cs

---
+++ MauiProgram.cs
@@ -0,0 +1,32 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.Logging;
+using VehicleDealAnalyzer.Services;
+using VehicleDealAnalyzer.ViewModels;
+using VehicleDealAnalyzer.Views;
+
+namespace VehicleDealAnalyzer;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        // Register Services
+        builder.Services.AddSingleton<DatabaseService>();
+        builder.Services.AddSingleton<NhtsaService>();
+
+        // Register ViewModel and Pages
+        builder.Services.AddSingleton<MainViewModel>();
+        builder.Services.AddSingleton<SavedDealsPage>();
+
+        return builder.Build();
+    }
+}
